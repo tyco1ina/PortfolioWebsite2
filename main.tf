a18d5529e0839b573cf4e460df3aab8f5c0c74c8
Tiberius Colina <tiberiuscolina@tiberiuss-air.myfiosgateway.com>
2025-01-04 13:20:16 -0500

    got rid of states bucket

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 provider "aws" {
-  region = "us-east-1"  // any region you want your aws configured to
+  region = "us-east-1" // any region you want your aws configured to
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "ty-portfolio-website" 
+  bucket = "ty-portfolio-website"
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id 
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false  
+  bucket                  = aws_s3_bucket.example.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 data "aws_iam_policy_document" "example" {
-  statement{
+  statement {
 
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = ["*"]
-    }    
+    }
 
     effect = "Allow"
     actions = [
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "example" {
     ]
 
     resources = [
-      aws_s3_bucket.example.arn ,
+      aws_s3_bucket.example.arn,
       "${aws_s3_bucket.example.arn}/*"
     ]
   }
@@ -37,7 +37,7 @@ data "aws_iam_policy_document" "example" {
 }
 
 resource "aws_s3_bucket_policy" "example" {
-  bucket = aws_s3_bucket.example.id 
+  bucket = aws_s3_bucket.example.id
   policy = data.aws_iam_policy_document.example.json
 
   depends_on = [
@@ -46,8 +46,8 @@ resource "aws_s3_bucket_policy" "example" {
 }
 
 resource "aws_s3_bucket_website_configuration" "example" {
-  bucket = aws_s3_bucket.example.id 
+  bucket = aws_s3_bucket.example.id
   index_document {
-    suffix = "index.html"    
+    suffix = "index.html"
   }
 }
